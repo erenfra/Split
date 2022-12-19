429e84197b2a18eee9dc6c0c172de540ed25bf9a
Renato Oliveira Fraga <rofraga74@gmail.com>
2022-12-19 10:30:44 -0600

    Initial Commit

---
+++ Split/SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitApp.swift
+//  Split
+//
+//  Created by Renato Oliveira Fraga on 12/19/22.
+//
+
+import SwiftUI
+
+@main
+struct SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
